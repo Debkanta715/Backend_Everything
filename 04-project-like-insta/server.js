76dfcfdfefca790dfd34b0051a1fb94e8bcc2791
Debkanta <debkantadey29@gmail.com>
2026-03-25 19:12:21 +0530

    Done

--- 04-project-like-insta/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require("./src/App");
-const connectDB = require("./src/database/DB");
-
-connectDB();
-
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
