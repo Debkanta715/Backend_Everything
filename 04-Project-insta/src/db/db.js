b5b1e83935020c6bde9ef9df28e04d6dc76ff9f1
Debkanta <debkantadey29@gmail.com>
2026-03-25 19:18:58 +0530

    done

--- 04-Project-insta/src/db/db.js
+++ 04-Project-insta/src/db/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  await mongoose.connect("mongodb+srv://yt:PFsDio394LwdmmkE@firstproject.eieqkdx.mongodb.net/data-1")
+  await mongoose.connect(
+    "mongodb+srv://yt:PFsDio394LwdmmkE@firstproject.eieqkdx.mongodb.net/",
+  );
   console.log("connect to db");
-  
-
 }
 
-
-module.exports=connectDB;
+module.exports = connectDB;
